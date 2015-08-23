57efa927607aaa5d1a67638e268dec1b6a397350
Louis-Philippe Véronneau <pollito@riseup.net>
2015-08-23 14:51:23 +0200

    Revert "spam_stats() merged with print_stats()"
    This reverts commit 75d096a9410cf9b016e6bed948421fac8d2f8306.
    
    spam_stats() & print_stats() actually need to be seperate functions

--- isbg.py
+++ isbg.py
@@ -640,6 +640,12 @@ def check_if_spam():
 pastuids, spamdeletelist, spamlist = check_if_spam()
 
 
+def spam_stats():
+    nummsg = len(uids)
+    spamdeleted = len(spamdeletelist)
+    numspam = len(spamlist) + spamdeleted
+
+
 # If we found any spams, now go and mark the original messages
 def mark_as_spam():
     if numspam or spamdeleted:
@@ -694,9 +700,6 @@ logout()
 
 
 def print_stats():
-    nummsg = len(uids)
-    spamdeleted = len(spamdeletelist)
-    numspam = len(spamlist) + spamdeleted
     if opts["--nostats"] is False:
         if opts["--learnspambox"] is not None:
             print("%d/%d spams learnt") % (s_learnt, s_tolearn)
